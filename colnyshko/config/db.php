386e7fa887a8a4a6dbae269285068887a2c636de
evschankin <evschankin555@gmail.com>
2023-05-14 23:27:50 +0300

    новое: установил yii2-authclient расширение

--- colnyshko/config/db.php
+++ colnyshko/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=colnyshko',
+    'username' => 'user_colnyshko',
+    'password' => 'C600d#$75F4',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
