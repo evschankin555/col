30aa388bca460ff67114e4f03bbd2371e1155498
evschankin <evschankin555@gmail.com>
2024-02-10 19:01:47 +0300

    feat: card to left

--- colnyshko/views/user/card.php
+++ colnyshko/views/user/card.php
@@ -7,11 +7,16 @@ use app\assets\UserPageAsset;
 UserPageAsset::register($this);
 $countSubscribers = $userCard->getFormattedSubscribersCountCard();
 ?>
-<div class="card card-page border-info mb-2">
+<div class="card border-info mb-2" style="max-width: 450px!important;">
     <div id="card-container" class="card-body">
         <div class="row">
-            <div class="col-md-6">
-                <div>
+            <div class="col-md-12">
+                <div class="mb-3 card-title">
+                    <h2 class="postcard-title"><?=$imageRelation->title?></h2>
+                </div>
+
+
+                <div class="mb-3">
                     <div class="card-container-image">
                         <img src="<?=$image->url?>" alt="Uploaded Image">
                         <div class="user-username-images">
@@ -21,17 +26,17 @@ $countSubscribers = $userCard->getFormattedSubscribersCountCard();
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6">
-                <div class="mb-3 card-title">
-                    <h2 for="postcard-title" ...=""><?=$imageRelation->title?></h2>
-                </div>
                 <div class="mb-3 card-desc">
                     <span><?=$imageRelation->description?></span>
                 </div>
                 <div class="mb-3 btns">
-                    <a type="button" class="btn btn-outline-primary btn-sm" title='Коллекция "<?=$collection->name?>"'><?=$collection->name?></a>
-                    <a type="button" class="btn btn-outline-info btn-sm" title='Категория "<?=$category->name?>"'><?=$category->name?></a>
+                    <?php if ($collection): ?>
+                        <a type="button" class="btn btn-outline-primary btn-sm" title='Коллекция "<?=$collection->name?>"'><?=$collection->name?></a>
+                    <?php endif; ?>
+
+                    <?php if ($category): ?>
+                        <a type="button" class="btn btn-outline-info btn-sm" title='Категория "<?=$category->name?>"'><?=$category->name?></a>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
